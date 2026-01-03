4f86512b266aa7e0bd23a35493b28a3ddc156241
Harshitha M <mharshitha641@gmail.com>
2026-01-03 19:50:09 +0530

    third commit

--- src/test/java/tests/AddProductToWishlistTest.java
+++ src/test/java/tests/AddProductToWishlistTest.java
@@ -13,12 +13,12 @@ public class AddProductToWishlistTest extends Basetest{
 	    public void addProductToWishlist() throws InterruptedException {
 
 	        HomePage homePage = new HomePage(driver);
-	        homePage.searchProduct("Canon EOS");
-	        homePage.addProductToWishlist("Canon EOS");
+	        homePage.searchProduct("Canon EOS 5D");
+	        homePage.addProductToWishlist("Canon EOS 5D");
 
 	        Thread.sleep(2000); // wait for success message
 	        String message = homePage.getSuccessMessage();
-	        Assert.assertTrue(message.contains("Canon EOS"), "Wishlist message not displayed correctly");
+	        Assert.assertTrue(message.contains("Canon EOS 5D"), "Wishlist message not displayed correctly");
 
 	        Reporter.log("AddProductToWishlistTest executed successfully", true);
 	    }
